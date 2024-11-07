58408d6086df1ee307db4c1bb7a62aa3d1535e53
João Vitor <jvlima0002020@gmail.com>
2024-11-06 21:38:03 -0300

    create field image in api

--- database/migrations/2024_10_12_035138_create_bookstore.php
+++ database/migrations/2024_10_12_035138_create_bookstore.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('bookstore', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
             $table->string('title');
             $table->string('author');
             $table->string('isbn');
